7d83595c08ac9c53b8ac0980950dcc67a23ffb7e
A7med-sharaf <a9986939@gmail.com>
2024-09-01 03:45:05 +0300

    task  js

---
+++ index.js
@@ -0,0 +1,16 @@
+let inputvalue=document.querySelectorAll('input[type="radio"]');
+
+
+
+console.log(inputvalue);
+
+function sele(){
+    if(inputvalue[0] == false){
+        console.log("hello");
+        
+    }else{
+        console.log("lo");
+        
+    }
+}
+sele()  
